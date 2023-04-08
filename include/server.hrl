0a33157f7a4620a81c6d2cee79cd31fba82a247a
Levi Siuzdak <levi@lvsz.dk>
2023-04-09 00:59:14 +0200

    Minor modifications to headers
    1. Added default name to server record
    2. Fixed function imported from comms.erl

--- include/server.hrl
+++ include/server.hrl
@@ -7,11 +7,12 @@
 %%--------------------------------------------------------------------------
 
 -record(server, {
-    id :: server:id(),
+    id = none :: server:id(),
     users = account:new_db() :: account:db(),
     active = dict:new() :: dict:dict(pid(), account:id()),
     channel = comms:new_channel(server) :: comms:channel()
 }).
 
 %%--------------------------------------------------------------------------
+
 -endif.
