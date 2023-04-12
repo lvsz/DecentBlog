bfb6848072b2471d34527fd21be16e74cc6deacf
Levi Siuzdak <levi@lvsz.dk>
2023-04-12 02:29:19 +0200

    Added password type
    Signed-off-by: Levi Siuzdak <levi@lvsz.dk>

--- src/pw.erl
+++ src/pw.erl
@@ -1,15 +1,18 @@
 -module(pw).
 -export([sign/2, verify/2]).
+-export_type([pw/0]).
 -include("account.hrl").
 
 %-spec init(ServerName) -> no_return().
 %init(ServerName) -> .
+
+-type pw() :: binary().
 
 encode(Text) -> crypto:hash(blake2s, Text).
 
 -spec sign(Account :: #account{}, PlainText :: string()) -> #account{}.
 sign(Account, PlainText) -> Account#account{password = encode(PlainText)}.
 
--spec verify(Account :: #account{}, Password :: binary()) -> boolean().
+-spec verify(Account :: #account{}, Password :: pw()) -> boolean().
 verify(#account{password = Password}, Password) -> true;
 verify(_Account, _Password) -> false.
