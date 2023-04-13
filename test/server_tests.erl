9a23891f026c385ee3db7a5bfb099c3b104049c3
Levi Siuzdak <levi@lvsz.dk>
2023-04-13 13:23:18 +0200

    Test server data creation
    Signed-off-by: Levi Siuzdak <levi@lvsz.dk>

--- test/server_tests.erl
+++ test/server_tests.erl
@@ -7,6 +7,13 @@
 -include_lib("kernel/include/logger.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+new_test() ->
+    ID = ?FUNCTION_NAME,
+    ?assertMatch(
+        #{id := ID, users := _, active := #{}, channel := _},
+        server:new(ID)
+    ).
+
 test_registration(Account, ServerID) ->
     ?assert(lists:member(ServerID, registered()), "is server reachable"),
     ServerID ! {self(), register, Account},
@@ -26,4 +33,3 @@ test_registration(Account, ServerID) ->
     UID = Account#account.id,
     ?assertMatch({ok, #account{id = UID, server = ServerID}}, Response),
     Response.
-
